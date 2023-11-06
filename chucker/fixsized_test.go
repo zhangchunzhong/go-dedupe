919016ade05322cdfbbead689ebcd231aab0f74b
zhangchunzhong <zczxyz@126.com>
2023-11-06 21:42:27 +0800

    add chunker test

---
+++ chucker/fixsized_test.go
@@ -0,0 +1,47 @@
+package chunker
+
+import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"testing"
+)
+
+func TestFixSizedCDC(t *testing.T) {
+
+	data := make([]byte, 1000*1024*1024)
+	rand.Seed(4542)
+	rand.Read(data)
+	rd := bytes.NewReader(data)
+
+	chunker, err := NewFixedSizeChunker(rd, 16*1024)
+	if err != nil {
+		log.Fatal(err)
+	}
+	m := make(map[string]int64)
+
+	t.Logf("%-32s  %s\n", "CHECKSUM", "CHUNK SIZE")
+
+	for {
+		chunk, err := chunker.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		mds := md5.Sum(chunk.Data)
+		key := fmt.Sprintf("%x", mds)
+		if val, ok := m[key]; ok {
+			m[key] = val + 1
+		} else {
+			m[key] = 1
+		}
+	}
+	for key, count := range m {
+		t.Logf("%s: %d\n", key, count)
+	}
+}
